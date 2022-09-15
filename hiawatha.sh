46056c8eca17620d3478775e2be1ab332e5fcdc4
arunodhayamsam <100224916+arunodhayamsam@users.noreply.github.com>
2022-09-15 13:20:51 +0530

    Update melange

--- hiawatha.sh
+++ hiawatha.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-apk add wget bash tar musl-dev gcc cmake make libtool libc6-compat pcre-dev  
-apk add cmake make musl-dev libxml2-dev libxslt-dev util-linux php7-json php7-fpm
 wget https://www.hiawatha-webserver.org/files/hiawatha-11.1.tar.gz
 tar -xzf hiawatha-11.1.tar.gz
 cd hiawatha-11.1
