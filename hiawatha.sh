bc857e306743c90f8f45f10e43c689ee74d29873
arunodhayamsam <100224916+arunodhayamsam@users.noreply.github.com>
2022-09-15 11:40:58 +0530

    Updates script

--- hiawatha.sh
+++ hiawatha.sh
@@ -1,9 +1,11 @@
-#/usr/bin/
-
-git clone https://github.com/arunodhayamsam/hiawatha-webserver.git 
-cd hiawatha-webserver 
-mkdir -p build && cd build 
-sleep 5
-cmake .. 
-sleep 5
+#!/usr/bin/env bash
+apk add wget bash tar musl-dev gcc cmake make libtool libc6-compat pcre-dev  
+apk add cmake make musl-dev libxml2-dev libxslt-dev util-linux php7-json php7-fpm
+wget https://www.hiawatha-webserver.org/files/hiawatha-11.1.tar.gz
+tar -xzf hiawatha-11.1.tar.gz
+cd hiawatha-11.1
+mkdir -p build && cd build
+cmake ..
 make install/strip
+/usr/local/sbin/hiawatha 
+hiawatha -k
