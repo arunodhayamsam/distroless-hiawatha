cd07c50693fad264892946ede91b9339f6a67e0b
arunodhayamsam <100224916+arunodhayamsam@users.noreply.github.com>
2022-09-16 11:05:38 +0530

    Update the melange

--- hiawatha.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-wget https://www.hiawatha-webserver.org/files/hiawatha-11.1.tar.gz
-tar -xzf hiawatha-11.1.tar.gz
-cd hiawatha-11.1
-mkdir -p build && cd build
-cmake ..
-make install/strip
-/usr/local/sbin/hiawatha 
-hiawatha -k
